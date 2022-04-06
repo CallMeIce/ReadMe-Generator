4fc0d65a72b618b27d9dec6d2829d15b572725a4
Isaak Morales <ice.isaak97@gmail.com>
2022-04-05 20:10:43 -0400

    Got it working but, have to get markdown syntax right

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ReadMe-Generator
+# no
+
+  ##Description no
+
+  ##Table of Contents Installation,Usage,Credits,License,Test
+
+  ##Installation no
+
+  ##Usage no
+
+  ##Collab no
+
+  ##Links no
+
+  ##license Mozilla
+
+  [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+
+  ##Tests no
+
+  ##Questions no
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer')
-
+const markdown = require('./generateMarkdown')
 // TODO: Create an array of questions for user input
+//* Title ====
+//* Description =====
+//* Table of Contents - Send you to corresponding section ====
+//* Installation ===
+//* Usage =====
+//* License - list ======
+//* Contributing =====
+//* Tests
+//*questions - link to github, and how to reach me email address=====
 
 inquirer
     .prompt([
@@ -17,12 +26,18 @@ inquirer
             message: "Provide a short description explaining the what, why, and how of your project.",
         },
         {
-            name: "steps",
+            name: "content",
+            type: "checkbox",
+            message: "List your table of contents",
+            choices: ["Installation", "Usage", "Credits", "License", "Test"],
+        },
+        {
+            name: "install",
             type: "input",
             message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running?",
         },
         {
-            name: "screenshot",
+            name: "usage",
             type: "input",
             message: "Provide instructions and examples for use. Include screenshots as needed.",
         },
@@ -36,28 +51,36 @@ inquirer
             type: "input",
             message: "Links to the deployed and the github repo.",
         },
+        {
+            name: "license",
+            type: "list",
+            message: "Pick your license",
+            choices: ["MIT", "Mozilla", "Unlicense"],
+        },
+        {
+            name: "test",
+            type: "input",
+            message: "Write any tests for your application",
+        },
+        {
+            name: "questions",
+            type: "input",
+            message: "How to reach me: Email and Github link:",
+        },
     ]).then((answer) => {
-        console.log(answer.description, answer.steps, answer.screenshot, answer.collab, answer.links);
-    
-        fs.writeFile("info.txt",JSON.stringify(answer, null, '\t'), (err) => err ? console.log(err) : console.log("Success"))
+        console.log(answer.title, answer.description, answer.content, answer.install, answer.usage, answer.collab, answer.links, answer.license, answer.test, answer.questions);
+
+        fs.writeFile("Readme.md", markdown({...answer}), (err) => err ? console.log(err) : console.log("Success"))
     });
 
 
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
 
-// const questions = {
-//     // one: "Provide a short description explaining the what, why, and how of your project.",
-//     // two: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
-//     // three: "Provide instructions and examples for use. Include screenshots as needed.",
-//     // four: "List your collaborators, if any, with links to their GitHub profiles, any third-party assets that require attribution, list the creators with links to their primary web presence in this section.",
-//     // five: "Links to the deployed and the github repo.",
 // }
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
 
-}
+// // TODO: Create a function to initialize app
+// function init() { }
 
-// TODO: Create a function to initialize app
-function init() { }
-
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
